36ba34080aeaeab8d2877ed3da61e063370ddd00
Ryusei Yamaguchi <mandel59@gmail.com>
2020-07-14 01:50:21 +0900

    Fix syntax error of bigm index

--- priv/repo/optional_migrations/bigm_indexing/20200714013200_add_bigm_fts_index_to_objects.exs
+++ priv/repo/optional_migrations/bigm_indexing/20200714013200_add_bigm_fts_index_to_objects.exs
@@ -3,12 +3,12 @@ defmodule Pleroma.Repo.Migrations.AddBigmFtsIndexToObjects do
 
   def up do
     execute("CREATE EXTENSION IF NOT EXISTS pg_bigm")
-    create_if_not_exists index(:objects, ["data->>'content' gin_bigm_ops"], using: :gin, name: :objects_fts_bigm)
+    create_if_not_exists index(:objects, ["(data->>'content') gin_bigm_ops"], using: :gin, name: :objects_fts_bigm)
     drop_if_exists index(:objects, ["(to_tsvector('english', data->>'content'))"], using: :gin, name: :objects_fts)
   end
 
   def down do
     create_if_not_exists index(:objects, ["(to_tsvector('english', data->>'content'))"], using: :gin, name: :objects_fts)
-    drop_if_exists index(:objects, ["data->>'content' gin_bigm_ops"], using: :gin, name: :objects_fts_bigm)
+    drop_if_exists index(:objects, ["(data->>'content') gin_bigm_ops"], using: :gin, name: :objects_fts_bigm)
   end
 end
